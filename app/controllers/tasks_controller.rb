ccdc18294f1ff2f56bf4ed594389fc6a2d3f81d7
Janiré Castillo <hello@articulategeek.com>
2020-05-01 23:37:17 -0400

    Added destroy action, form partial, and fixed redundancy.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+    before_action :set_task, only:[:edit, :update, :show, :destroy]
     def index
         @tasks = Task.all
 
@@ -9,7 +10,7 @@ class TasksController < ApplicationController
     end
 
     def edit
-        @task = Task.find(params[:id])
+        
     end
 
     def create
@@ -24,7 +25,7 @@ class TasksController < ApplicationController
     end
 
     def update
-        @task = Task.find(params[:id])
+        
         if @task.update(tasks_params)
             flash[:notice] = "Task was succesfully updated"
             redirect_to task_path(@task)
@@ -34,10 +35,21 @@ class TasksController < ApplicationController
     end
 
     def show
-        @task = Task.find(params[:id])
+        
+    end
+
+    def destroy
+        
+        @task.destroy
+        flash[:notice] = "Task was succesfully deleted."
+        redirect_to tasks_path
     end
 
     private
+
+    def set_task
+        @task = Task.find(params[:id])
+    end
 
     def tasks_params
         params.require(:task).permit(:content)
